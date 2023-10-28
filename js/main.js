0b883d9b01c192fcd4e233c34829742511dba2c0
chojeongeun <juy0816@naver.com>
2023-10-28 09:39:22 +0900

    anime 연습모션

---
+++ js/main.js
@@ -0,0 +1,46 @@
+const main = document.querySelector('main');
+const [topEl, rightEl, bottomEl, leftEl, conEl] = main.children;
+const btn = document.querySelector('button');
+const speed = 500;
+//먼저 필요한 함수 가져오기
+
+btn.addEventListener('click', () => {
+	new Anime(
+		topEl,
+		{ width: '100%' },
+		{
+			duration: 500,
+			callback: () => {
+				new Anime(
+					rightEl,
+					{ height: '100%' },
+					{
+						duration: speed,
+						callback: () => {
+							new Anime(
+								bottomEl,
+								{ width: '100%' },
+								{
+									duration: speed,
+									callback: () => {
+										new Anime(
+											leftEl,
+											{ height: '100%' },
+											{
+												duration: speed,
+												callback: () => {
+													//new Anime(conEl, { opacity: 1 }, { duration: speed });
+													conEl.classList.add('on');
+												},
+											}
+										);
+									},
+								}
+							);
+						},
+					}
+				);
+			},
+		}
+	);
+});
